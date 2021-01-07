17628310e235de6d4b31472c9d9b4cc688c4f0bf
DJ-AG <alekseigolovanov89@gmail.com>
2021-01-07 10:19:34 +0200

    created cart component and connected our cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 import Button from '../button/button.componen'
+import CartItem from '../cart-item/cart-item.component'
 
 import './cart-dropdown.style.scss'
 
 
-const CartDropDown = () => (
+
+const CartDropDown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+        {
+            cartItems.map(cartItem => <CartItem key= {cartItem.id} item={cartItem}/>)
+        }
+        </div>
         <Button>GO TO CHECKOUT</Button>
     </div>
 )
 
-export default CartDropDown
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems,
+})
+
+export default connect(mapStateToProps,null)(CartDropDown)
---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./cart-item.style.scss";
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <div className="cart-item">
+    <img src={imageUrl} alt="Item" />
+    <div className="item-details">
+      <span className="name">{name}</span>
+      <span className="price">
+        {quantity} x €{price}
+      </span>
+    </div>
+  </div>
+);
+
+export default CartItem
